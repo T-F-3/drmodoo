356062e058f0db257789e23fd3384f84cd8e6a65
kim-hyeona991113 <kim-hyeona991113@github.com>
2022-12-20 17:05:01 +0900

    파일 업데이트

--- brand/brand_reorga.js
+++ brand/brand_reorga.js
@@ -1,3 +1,30 @@
+   
+//  헤더 js
+
+(function headerSlide() {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    const headerBottomNeBtn1 = document.querySelector('.Headimgbox .siled1');
+    const headerBottomNeBtn2 = document.querySelector('.Headimgbox .siled2');
+
+
+
+    headerBottomNeBtn1.addEventListener('click', () => {
+        headerTopImg.classList.remove('ative');
+        headerTopImg.classList.add('ative');
+    })
+    headerBottomNeBtn2.addEventListener('click', () => {
+        headerTopImg.classList.add('ative');
+        headerTopImg.classList.remove('ative');
+    });
+})();
+// setInterval(콜백함수, 시간);
+setInterval(function () {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    headerTopImg.classList.toggle('ative');
+}, 4000);
+   
+   
+//    메뉴바+프로모션 창 js
    function Promotion(){
         const AtopPromotionBtn = document.querySelector('.x');
         const AtopPromotion = document.querySelector('.top_promotion')
