eea0634b9be3d2b9696bdfbe58f22446b9ed92ed
kim-hyeona991113 <kim-hyeona991113@github.com>
2022-12-21 15:27:13 +0900

    12월 21일 업데이트

--- brand/brand_ourstory.js
+++ brand/brand_ourstory.js
@@ -18,9 +18,27 @@
             scrollY > 90 ? Navbg.style.width = '100%' : Navbg.style.width = '50%';
 
      });
-    
-    $(function() {
-    $(".brand_label").simplyScroll({
-        pauseOnHover:false
+
+
+
+(function headerSlide() {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    const headerBottomNeBtn1 = document.querySelector('.Headimgbox .siled1');
+    const headerBottomNeBtn2 = document.querySelector('.Headimgbox .siled2');
+
+
+
+    headerBottomNeBtn1.addEventListener('click', () => {
+        headerTopImg.classList.remove('ative');
+        headerTopImg.classList.add('ative');
+    })
+    headerBottomNeBtn2.addEventListener('click', () => {
+        headerTopImg.classList.add('ative');
+        headerTopImg.classList.remove('ative');
     });
-});
+})();
+// setInterval(콜백함수, 시간);
+setInterval(function () {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    headerTopImg.classList.toggle('ative');
+}, 4000);
--- brand/brand_scalplab.js
+++ brand/brand_scalplab.js
@@ -19,8 +19,50 @@
 
      });
     
-    $(function() {
-    $(".brand_label").simplyScroll({
-        pauseOnHover:false
+     
+// 텍스트 효과
+
+
+     addEventListener('scroll', () => {
+        const txth2 = document.querySelector('.main_scalp_lab h2');
+        const txtp = document.querySelector('.main_scalp_lab p');
+
+           if(scrollY>500){
+                txth2.style.opacity = '1'
+                if(scrollY >= 600){
+                    txth2.style.transform = 'translateY(0px)'
+                }else{
+                    txth2.style.transform = 'translateY(-90px)'
+                }
+            }else{
+                txth2.style.opacity = '0'
+            } 
+
+            console.log(scrollY)
+
+     });
+    
+
+
+//    헤더 js
+(function headerSlide() {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    const headerBottomNeBtn1 = document.querySelector('.Headimgbox .siled1');
+    const headerBottomNeBtn2 = document.querySelector('.Headimgbox .siled2');
+
+    headerBottomNeBtn1.addEventListener('click', () => {
+        headerTopImg.classList.remove('ative');
+        headerTopImg.classList.add('ative');
+    })
+    headerBottomNeBtn2.addEventListener('click', () => {
+        headerTopImg.classList.add('ative');
+        headerTopImg.classList.remove('ative');
     });
-});
+})();
+// setInterval(콜백함수, 시간);
+setInterval(function () {
+    const headerTopImg = document.querySelector('.Headimgbox .img2');
+    headerTopImg.classList.toggle('ative');
+}, 4000);
+
+
