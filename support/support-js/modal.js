75254c1f67fc632878dc8b1dc58e4e5a9fe01247
myamta <kjy6478@naver.com>
2022-12-22 12:34:31 +0900

    사이드바 추가

---
+++ support/support-js/modal.js
@@ -0,0 +1,34 @@
+const xbtn = document.querySelector('.xmark');
+const gybbpr = document.querySelector('.gybbpr');
+const counseApp = document.querySelector('.counseApp');
+const naverBtn = document.querySelector('.naver_btn');
+const phone = document.querySelector('.phone_box');
+const sidebar = document.querySelector('#sidebar_container');
+const sidebarBtn = document.querySelector('.side_btn');
+
+gybbpr.addEventListener('click', () => {
+   counseApp.classList.add('on');
+   gybbpr.classList.add('off');
+});
+xbtn.addEventListener('click', () => {
+   counseApp.classList.remove('on');
+   gybbpr.classList.remove('off');
+});
+
+naverBtn.addEventListener('mouseover', () => {
+   document.getElementById('naver_talk').style.display = 'block';
+});
+naverBtn.addEventListener('mouseout', () => {
+   document.getElementById('naver_talk').style.display = 'none';
+});
+
+phone.addEventListener('mouseover', () => {
+   document.getElementById('phone_num').style.display = 'block';
+});
+phone.addEventListener('mouseout', () => {
+   document.getElementById('phone_num').style.display = 'none';
+});
+
+sidebarBtn.addEventListener('click', () => {
+   sidebar.classList.toggle('open');
+});
