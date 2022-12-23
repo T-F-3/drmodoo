2dfeeccb9891286be0dd121c3167d455c5f9bbd3
LJaeeun <tpdls082@naver.com>
2022-12-23 14:16:14 +0900

    index완성

--- lje/index.js
+++ lje/index.js
@@ -30,12 +30,10 @@ Promotion();
 
 
     headerBottomNeBtn1.addEventListener('click', () => {
-        headerTopImg.classList.remove('ative');
-        headerTopImg.classList.add('ative');
+        headerTopImg.classList.toggle('ative');
     })
     headerBottomNeBtn2.addEventListener('click', () => {
-        headerTopImg.classList.add('ative');
-        headerTopImg.classList.remove('ative');
+        headerTopImg.classList.toggle('ative');
     });
 })();
 // setInterval(콜백함수, 시간);
@@ -47,18 +45,68 @@ setInterval(function () {
 
 
 /* ----------main_swiper--------------- */
-
-let swiper = new Swiper(".swiper-container", {
-    speed: 600,
+let mainSswiper = new Swiper(".swiper1", {
+    speed: 400,
     effect : 'fade',
     parallax: true,
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: false,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#main_swiper .swiper-button-next",
+        prevEl: "#main_swiper .swiper-button-prev",
     },
 });
 
+
+let mainreview = new Swiper("#swiper2", {
+        loop: false,
+        autoplay: false,
+        slidesPerView: 3.5,
+        spaceBetween: 15,
+        slidesPerGroup: 3,
+        freeMode: true,
+        observer: true,	// 추가
+        observeParents: true,	// 추가
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+        navigation: {
+            nextEl: '#main_review .swiper-button-next',
+            prevEl: '#main_review .swiper-button-prev',
+        },
+});
+
+/* anination */
+//2537 main reo
+//3425 main scalp
+addEventListener('scroll', () => {
+    const ani1 = document.querySelector('.reorgaTxt p');
+    const ani2 = document.querySelector('.scalplabTxt p');
+
+    if(scrollY>2537){
+        ani1.style.opacity = '1'
+        if(scrollY >= 2600){
+            ani1.style.transform = 'translateY(0px)'
+        }else{
+            ani1.style.transform = 'translateY(-120px)'
+        }
+    }else{
+        ani1.style.opacity = '0'
+    } 
+    if(scrollY>3300){
+        ani2.style.opacity = '1'
+        if(scrollY >= 3440){
+            ani2.style.transform = 'translateY(0px)'
+        }else{
+            ani2.style.transform = 'translateY(-120px)'
+        }
+    }else{
+        ani2.style.opacity = '0'
+    } 
+    console.log(scrollY)
+
+    
+})
