35d13bd6d70148adfe8533bb8cba50d4e1988d0d
kim-hyeona991113 <kim-hyeona991113@github.com>
2022-12-23 14:29:11 +0900

    진짜 끝git add .

--- brand/brand_reorga.js
+++ brand/brand_reorga.js
@@ -1,22 +1,4 @@
    
-//  헤더 js
-
-(function headerSlide() {
-    const headerTopImg = document.querySelector('.Headimgbox .img2');
-    const headerBottomNeBtn1 = document.querySelector('.Headimgbox .siled1');
-    const headerBottomNeBtn2 = document.querySelector('.Headimgbox .siled2');
-
-
-
-    headerBottomNeBtn1.addEventListener('click', () => {
-        headerTopImg.classList.remove('ative');
-        headerTopImg.classList.add('ative');
-    })
-    headerBottomNeBtn2.addEventListener('click', () => {
-        headerTopImg.classList.add('ative');
-        headerTopImg.classList.remove('ative');
-    });
-})();
 
 // setInterval(콜백함수, 시간);
 setInterval(function () {
@@ -62,3 +44,5 @@ for(let i =0; i < certificateBtn.length; i++){
 
   })
 }
+
+
