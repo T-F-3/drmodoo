ecafd784006bc36e4021f1523e489000f7990615
kim-hyeona991113 <kim-hyeona991113@github.com>
2022-12-27 12:15:12 +0900

    js수정

--- js/brand-js/brand_reorga.js
+++ js/brand-js/brand_reorga.js
@@ -5,17 +5,15 @@ const certificateBtn = document.querySelectorAll('.material-symbols-outlined')
 const certificateIn = document.querySelectorAll('.Complex_txt div')
 
 
- for(let i =0; i < certificateBtn.length; i++){
+  for(let i =0; i < certificateBtn.length; i++){
   certificateBtn[i].addEventListener('click',function(e){
+     certificateIn.forEach(function (e) {
+    e.classList.remove("active");
+  });
     certificateBtn[i].classList.toggle('active');
     certificateIn[i-1].classList.toggle('active');
-
   })
 }
-
-
-
-
 
 
 
