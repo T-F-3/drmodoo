74ec647241175bf587323784ea48023e67276340
yis-park <ckadltmf423@naver.com>
2022-12-27 22:23:41 +0900

    shop js시도,,,

--- js/shop-js/product.js
+++ js/shop-js/product.js
@@ -1,14 +1,8 @@
-const select = document.querySelector("select");
 const option = document.querySelector("option");
-const result = document.querySelector("p");
-const right = document.querySelector(".right");
+const select = document.querySelector("select");
+const product = document.querySelector(".SaleProduct_1t");
+const p = document.querySelector(".SaleProduct_1 tr td p");
 
-option.addEventListener("click", (e) => {
-  e.preventDefault();
-  if ((value = "P000000O000G")) {
-    result.classList.add = "toggle";
-    right.style.display = "block";
-  } else {
-    alert("옵션을 선택하세요.");
-  }
+option.addEventListener("click", () => {
+  p.style.display = "block";
 });
